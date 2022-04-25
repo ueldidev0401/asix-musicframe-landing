2d08702a4b15403c8000e183fd08a5e882bb68ee
Amon I <101267106+horseking82@users.noreply.github.com>
2022-04-25 18:53:00 +0200

    testing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from 'next-i18next';
 
-import LocalePicker from 'components/LocalePicker'
-import NavPopup from 'components/NavPopup'
-import StyledLink from 'components/StyledLink'
-import IconClose from 'public/img/iconClose.svg'
-import IconHedgehog from 'public/img/iconHedgehog.svg'
-import IconKebabHorizontal from 'public/img/iconKebabHorizontal.svg'
-import IconNavigationApi from 'public/img/iconNavigationApi.svg'
-import IconNavigationCareer from 'public/img/iconNavigationCareer.svg'
-import IconNavigationDocs from 'public/img/iconNavigationDocs.svg'
-import IconNavigationRunningNode from 'public/img/iconNavigationRunningNode.svg'
-import IconNavigationStaking from 'public/img/iconNavigationStaking.svg'
-import IconNavigationTeam from 'public/img/iconNavigationTeam.svg'
-import IconNavigationTokenomics from 'public/img/iconNavigationTokenomics.svg'
-import IconNavigationWhitepaper from 'public/img/iconNavigationWhitepaper.svg'
+
+import IconClose from 'public/img/iconClose.svg';
+import IconHedgehog from 'public/img/iconHedgehog.svg';
+import IconKebabHorizontal from 'public/img/iconKebabHorizontal.svg';
+import IconNavigationApi from 'public/img/iconNavigationApi.svg';
+import IconNavigationCareer from 'public/img/iconNavigationCareer.svg';
+import IconNavigationDocs from 'public/img/iconNavigationDocs.svg';
+import IconNavigationRunningNode from 'public/img/iconNavigationRunningNode.svg';
+import IconNavigationStaking from 'public/img/iconNavigationStaking.svg';
+import IconNavigationTeam from 'public/img/iconNavigationTeam.svg';
+import IconNavigationTokenomics from 'public/img/iconNavigationTokenomics.svg';
+import IconNavigationWhitepaper from 'public/img/iconNavigationWhitepaper.svg';
+
+
+import LocalePicker from 'components/LocalePicker';
+import NavPopup from 'components/NavPopup';
+import StyledLink from 'components/StyledLink';
 
 const renderIcon = (Icon: any) => () =>
   <Icon height={21} width={21} className='icon' />
